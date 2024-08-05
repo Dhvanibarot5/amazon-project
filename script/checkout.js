38d72a7b266e19f8efc4dd1de0ecb673d0a1e377
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-08-06 04:51:08 +0530

    radio selector fixing

--- script/checkout.js
+++ script/checkout.js
@@ -19,7 +19,7 @@ cart.forEach((cartItem) => {
 
               <div class="cart-item-details">
                 <div class="product-name">${matchingProduct.name}</div>
-                <div class="product-price">$${matchingProduct.priceCents / 100}</div>
+                <div class="product-price">$${(matchingProduct.priceCents / 100).toFixed(2)}</div>
                 <div class="product-quantity">
                   <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
                   <span class="update-quantity-link link-primary"> Update </span>
@@ -30,21 +30,21 @@ cart.forEach((cartItem) => {
               <div class="delivery-options">
                 <div class="delivery-options-title">Choose a delivery option:</div>
                 <div class="delivery-option">
-                  <input type="radio" checked class="delivery-option-input" name="delivery-option-1" />
+                  <input type="radio" checked class="delivery-option-input" name="delivery-option-${matchingProduct.id}" />
                   <div>
                     <div class="delivery-option-date">Tuesday, June 21</div>
                     <div class="delivery-option-price">FREE Shipping</div>
                   </div>
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input" name="delivery-option-1" />
+                  <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}" />
                   <div>
                     <div class="delivery-option-date">Wednesday, June 15</div>
                     <div class="delivery-option-price">$4.99 - Shipping</div>
                   </div>
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input" name="delivery-option-1" />
+                  <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}" />
                   <div>
                     <div class="delivery-option-date">Monday, June 13</div>
                     <div class="delivery-option-price">$9.99 - Shipping</div>
