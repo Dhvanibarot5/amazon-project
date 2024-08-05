6692208cd7a457bcd6608fc3d05f46d2803d3864
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-08-05 17:37:05 +0530

    first commit

---
+++ data/amazon.js
@@ -0,0 +1,28 @@
+const product = [
+  {
+    image: `images/products/athletic-cotton-socks-6-pairs.jpg`,
+    name: `Black and Gray Athletic Cotton Socks - 6 Pairs`,
+    rating: {
+      star: 4.5,
+      count: 87,
+    },
+    priceCents: 1090,
+  },
+  {
+    image: `images/products/intermediate-composite-basketball.jpg`,
+    name: `Intermediate Size Basketball`,
+    rating: {
+      star: 4,
+      count: 127,
+    },
+    priceCents: 2095,
+  },
+  {
+    image: `images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg`,
+    name: `Adults Plain Cotton T-Shirt - 2 Pack`,
+    rating: {
+      star: 4.5,
+      count: 56,
+    },
+    priceCents: 799,},
+];
